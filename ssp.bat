301f06d54a18c577475cfd0414b2d3a430ec432a
Smealm <46572305+Smealm@users.noreply.github.com>
2024-07-10 10:42:23 -0400

    Update ssp.bat

--- ssp.bat
+++ ssp.bat
@@ -1,3 +1,7 @@
+Credits:
+Myself
+[ChatGPT](https://chatgpt.com/) for teaching me how to do things I had trouble figuring out
+
 :: --------------------------------------------------------------------------------------------
 cls
 
