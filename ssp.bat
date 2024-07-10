ef6e2449eb760aae60d25ee75c2758e171b1d33e
Smealm <46572305+Smealm@users.noreply.github.com>
2024-07-10 10:35:26 -0400

    Update ssp.bat

--- ssp.bat
+++ ssp.bat
@@ -1,17 +1,18 @@
+:: --------------------------------------------------------------------------------------------
+cls
+
+:: UPDATE PROGRAMS WITH STATIC URL's HERE
+
+
 :: NVIDIA GEFORCE EXPERIENCE INFORMATION
 :: Geforce Experience Version
 set NvidiaVersion="3.28.0.417"
 
-:: AMD ADRENALIN INFORMATION
-:: Adrenalin Version
-set AMDVersion="24.6.1"
-:: Installer Version
-set AMDInstall="240626"
-
-
 :: AME WIZARD INFORMATION
 :: AME Wizard Version
 set AMEVersion="0.7.5"
+:: AME Wizard Build
+set AMEBuild="Beta"
 
 :: ReviOS INFORMATION
 :: ReviOS Version
@@ -26,10 +27,17 @@ set AtlasOSVersion="0.4.0"
 set AMEPlaybookVer="0.7"
 
 
+:: --------------------------------------------------------------------------------------------
+cls
+
+:: CHECK CERTAIN THINGS BEFORE RUNNING MAIN SCRIPT
+
+
+@echo off
 :: Go to code that checks if script is running on a Windows 10 or a Windows 11 system
 :: and if it isn't then notify user, await input and close script
 goto PreWindowsVersionCheck
-:failed
+:WindowsVersionCheckFailed
 @echo Unsupported Windows Version
 Pause
 exit
@@ -40,19 +48,44 @@ for /f "tokens=4-5 delims= " %%i in ('ver') do set VERSION=%%i %%j
 :: Check if the version contains "10." (for Windows 10) or "11." (for Windows 11)
 echo %VERSION% | findstr /C:"10." > nul
 if %errorlevel% equ 0 (
-goto success
+goto AdminCheckStart
 ) else (
 echo %VERSION% | findstr /C:"11." > nul
 if %errorlevel% equ 0 (
-goto success
+goto AdminCheckStart
 ) else (
-goto failed
+goto WindowsVersionCheckFailed
 )
 )
-:success
-:: Create "ssp" directory
-mkdir ssp
+:AdminCheckSTART
+:: Check for Admin Privileges
+openfiles >nul 2>&1
+if '%errorlevel%' NEQ '0' (
+    :: Inform the user about missing Administrator privileges
+	cls
+    echo This script is not running as administrator.
+    echo Some features may not be available.
+    pause
+) else (
+    goto AdminCheckEND
+)
+:AdminCheckEND
+cls
+:: Checking if windows is activated
+@echo checking if Windows is activated
+setlocal
 
+:: Check Windows Activation Status
+for /f "tokens=*" %%i in ('slmgr.vbs /xpr') do set "activationStatus=%%i"
+
+:: Output the activation status for debugging purposes
+echo %activationStatus%
+
+:: Check if the activation status contains the phrase indicating Windows is activated
+echo %activationStatus% | findstr /i "The machine is permanently activated" >nul
+if %errorlevel% equ 0 (
+goto ActivateEND
+) else (
 cls
 :: Open activation script by pressing either 1 or 2
 choice /C 12 /M "Do you want to open the Windows Activation Script? 1 = YES, 2 = NO : "
@@ -60,11 +93,27 @@ choice /C 12 /M "Do you want to open the Windows Activation Script? 1 = YES, 2 =
 if errorlevel 2 goto ActivateEND
 :: Listen for keypress "1", if pressed, run script then move on
 if errorlevel 1 goto YESACTIVATE
-
 :YESACTIVATE
 :: Activate Windows
 powershell -c "irm https://massgrave.dev/get | iex"
+)
 :ActivateEND
+endlocal
+
+
+:: --------------------------------------------------------------------------------------------
+cls
+
+:: RUN MAIN SCRIPT
+
+:ScriptSTART
+
+
+:: --------------------------------------------------------------------------------------------
+cls
+
+:: INSTALL DEPENDENCIES
+
 
 cls
 :: Goes along with downloading, installing and deleting dependencies depending on user input
@@ -74,6 +123,8 @@ if errorlevel 2 goto DependenciesEnd
 :: Listen for keypress "1", if pressed, don't run script and continue
 if errorlevel 1 goto YesDependencies
 :YesDependencies
+:: Create "ssp" directory
+mkdir ssp
 
 :: Download OpenAL
 curl.exe -fSLo ssp.zip https://www.openal.org/downloads/oalinst.zip
@@ -113,6 +164,8 @@ echo !gpu_name! | findstr /i "NVIDIA" > nul
 if !errorlevel! equ 0 (
 echo Downloading Geforce Experience
 curl.exe -fSLo GeForce_Experience_v%NvidiaVersion%.exe https://us.download.nvidia.com/GFE/GFEClient/%NvidiaVersion%/GeForce_Experience_v%NvidiaVersion%.exe
+cls
+goto GPUDOWNLOADEND
 )
 
 :: If echoed GPU name includes "AMD" download Adrenalin
@@ -127,13 +180,14 @@ Pause
 start https://www.amd.com/en/support/download/drivers.html
 @echo Once you have completed the steps above
 Pause
+goto GPUDOWNLOADEND
 cls
 )
-
 )
+:GPUDOWNLOADEND
 :: Disables delayed expansion of variables (!variable! syntax)
 ENDLOCAL
-
+@echo running installers
 :: Run VisualCppRedist_AIO_x86_x64.exe and install
 VisualCppRedist_AIO_x86_x64.exe /y
 
@@ -161,32 +215,31 @@ if exist GeForce_Experience_v%NvidiaVersion%.exe (
 )
 :NvidiaPass
 
-:: Run AMD Adrenalin (if you have an AMD GPU)
-if exist amd-software-adrenalin-edition-%AMDVersion%-minimalsetup-%AMDInstall%_web.exe ( 
- amd-software-adrenalin-edition-%AMDVersion%-minimalsetup-%AMDInstall%_web.exe
- goto AMDPass
-) else (
- goto AMDPass
+setlocal
+:: Find the first file that matches the criteria.
+for /f "delims=" %%F in ('dir /b /a-d "%~dp0\ssp\amd-software-adrenalin-edition-*.exe" 2^>nul') do (
+    :: Execute the file.
+    start "" "%~dp0\ssp\%%F"
+    goto :AMDPass
 )
 :AMDPass
+endlocal
 
 :: Clean up
-del DotNetRuntime.exe
-del JavaInstall.exe
-del VisualCppRedist_AIO_x86_x64.exe
-del dxwebsetup.exe
-del oalinst.exe
-del xnafx40_redist.msi
-del GeForce_Experience_v%NvidiaVersion%.exe
-del amd-software-adrenalin-edition-%AMDVersion%-minimalsetup-%AMDInstall%_web.exe
-:DependenciesEnd
-cd ssp
+cd ..
+rmdir /s /q ssp
 
+:DependenciesEnd
+
+
+:: --------------------------------------------------------------------------------------------
 cls
+
+:: AME WIZARD AND PLAYBOOKS
 
 
 :: Download and run AME Wizard
-choice /C 123 /M "Do you want to go through the process of installing a playbook on your system? 1 = YES, 2 = NO, 3 = Learn More : "
+choice /C 123 /M "Do you want to download AME Wizard and go through the process of installing a playbook one your system? 1 = YES, 2 = NO, 3 = Learn More : "
 :: Listen for keypress "3", if pressed, open AME Wizard's Website and return
 if errorlevel 3 goto AMEWhat
 :: Listen for keypress "2", if pressed then move on
@@ -194,17 +247,19 @@ if errorlevel 2 goto AMEWizardEND
 :: Listen for keypress "1", if pressed then move on
 if errorlevel 1 goto AMEWizardYes
 
-
 :AMEWhat
 start https://ameliorated.io/
 goto :DependenciesEnd
 
 :AMEWizardYes
 cls
+mkdir ssp
+cd ssp
 :: Download AME Wizard
-curl.exe -fSLo AME-Wizard-Beta.exe https://github.com/Ameliorated-LLC/trusted-uninstaller-cli/releases/download/%AMEVersion%/AME-Wizard-Beta.exe
+curl.exe -fSLo AME-Wizard-v%AMEVersion%-%AMEBuild%.exe https://github.com/Ameliorated-LLC/trusted-uninstaller-cli/releases/download/%AMEVersion%/AME-Wizard-%AMEBuild%.exe
 :PlaybookSTART
 :: Download ReviOS or AtlasOS by pressing either 1 or 2
+cls
 choice /C 1234 /M "Which playbook would you like to download? 1 = Revision, 2 = Atlas, 3 = Ameliorated, 4 = Learn More : "
 if errorlevel 4 goto PlaybookWhat
 :: Listen for keypress "3", if pressed, Download AME Playbook then move on
@@ -244,8 +299,68 @@ goto PlaybookSTART
 :PlaybookEND
 
 :: Run AME Wizard
-AME-Wizard-Beta.exe
+cls
+@echo continue the setup through AME Wizard 
+@echo choose the playbook you downloaded earlier and 
+@echo go through the process of installing it through AME
+AME-Wizard-v%AMEVersion%-%AMEBuild%.exe
+
 :AMEWizardEND
+
+setlocal
+
+:: Define the name of the executable to wait for
+set "processName=AME-Wizard-v%AMEVersion%-%AMEBuild%.exe"
+
+:: Wait until the process is no longer running
+:AMEWizardLoopCheck
+@echo Waiting for %processName% to close...
+tasklist /FI "IMAGENAME eq %processName%" 2>NUL | findstr /I /C:"%processName%" >NUL
+if %ERRORLEVEL% == 0 (
+    echo %processName% is still running. Waiting...
+    timeout /t 1 /nobreak > NUL
+    goto waitForProcess
+)
+
+:: Execute the desired command
+cd ..
+rmdir /s /q ssp
+endlocal
+
+
+:: --------------------------------------------------------------------------------------------
+cls
+:: END OF SCRIPT COMMANDS
+
+
+@echo off
+:: Check if the script is running as Administrator
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    cls
+:: Restart Windows by pressing either 1 or 2
+choice /C 12 /M "Would you like to restart Windows? 1 = YES, 2 = NO : "
+:: Listen for keypress "2", if pressed, don't run script and continue
+if errorlevel 2 goto RestartWindowsEND
+:: Listen for keypress "1", if pressed, run script then move on
+if errorlevel 1 goto RestartWindowsConfirm
+
+:RestartWindowsConfirm
+cls
+choice /C 12 /M "Are you sure you want to restart Windows? 1 = I am Sure, 2 = On second thought no : "
+:: Listen for keypress "2", if pressed, don't run script and continue
+if errorlevel 2 goto RestartWindowsEND
+:: Listen for keypress "1", if pressed, run script then move on
+if errorlevel 1 goto RestartWindows
+
+:RestartWindows
+shutdown /r
+) else (
+    goto RestartWindowsEND
+)
+:RestartWindowsEND
+
+:ScriptEND
 
 @echo script finished
 cd ..
